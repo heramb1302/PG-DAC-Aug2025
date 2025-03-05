cd6056bdae5adf958a3923b9a385cbbbe41e849e
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2025-03-05 19:28:57 +0530

    Update InputDemo.java

--- OOPJ/Day 3/InputDemo.java
+++ OOPJ/Day 3/InputDemo.java
@@ -1,19 +1,11 @@
-class CmdArgs1 {
+class InputDemo {
     public static void main(String[] args) {
-		//int i =10;//compile time 
-		//user input: input required at run time
 		
-		String s1 = args[0];
-		String s2 = args[1];
-		System.out.println(s1);
-		System.out.println(s2);
 		
-		//Wrapper classes int=Integer, float = Float
-		int i = Integer.parseInt(s1);//10 converted string into int data type
-		int j = Integer.parseInt(s2);
-		
-		System.out.println(s1+s2);
-		System.out.println(i+j);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter ur name");
+		String nm = sc.next();
+		System.out.println("Welcome "+nm);
 		
 		
 		
